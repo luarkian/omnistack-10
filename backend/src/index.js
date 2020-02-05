1dc512b9ef14af9c1be3641c39b236d0c81f0a83
luarkian <kaype_x@hotmail.com>
2020-02-05 17:59:53 -0400

    frontend visual

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,4 +14,4 @@ app.use(express.json())
 app.use(routes)
 
 
-app.listen(3000)
+app.listen(3030)
--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude " required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+
+      </aside>
+      <main>
+        <ul>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/19172548?s=460&v=4" alt="Luarkian Kaype" />
+            <div className="user-infor">
+              <strong> Luarkian K</strong>
+              <span> React</span>
+            </div>
+          </header>
+          <p>dsfdsfsadasf</p>
+          <a href="https://github.com/luarkian/">Acesse o Github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/19172548?s=460&v=4" alt="Luarkian Kaype" />
+            <div className="user-infor">
+              <strong> Luarkian K</strong>
+              <span> React</span>
+            </div>
+          </header>
+          <p>dsfdsfsadasf</p>
+          <a href="https://github.com/luarkian/">Acesse o Github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/19172548?s=460&v=4" alt="Luarkian Kaype" />
+            <div className="user-infor">
+              <strong> Luarkian K</strong>
+              <span> React</span>
+            </div>
+          </header>
+          <p>dsfdsfsadasf</p>
+          <a href="https://github.com/luarkian/">Acesse o Github</a>
+        </li>
+        <li className="dev-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/19172548?s=460&v=4" alt="Luarkian Kaype" />
+            <div className="user-infor">
+              <strong> Luarkian K</strong>
+              <span> React</span>
+            </div>
+          </header>
+          <p>dsfdsfsadasf</p>
+          <a href="https://github.com/luarkian/">Acesse o Github</a>
+        </li>
+        </ul>
+      </main>
+
     </div>
   );
 }
